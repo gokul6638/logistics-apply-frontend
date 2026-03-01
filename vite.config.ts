cf345755e85b548e3ee99bf1c61c9b088fc900b8
K R Gokul <136432803+gokul6638@users.noreply.github.com>
2026-03-01 17:28:44 -0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gokul6638/logistics-apply-frontend/", // repo name for GitHub Pages
+  base: "/logistics-apply-frontend/", // repo name for GitHub Pages
   server: {
     proxy: {
       "/api": {
